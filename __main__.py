4f3db50f05fc8a98ea8958a4ab6a3c2682e66936
Makki3608 <wulin007@seznam.cz>
2026-02-23 13:14:10 +0100

    Added error if user exits file dialog.

--- __main__.py
+++ __main__.py
@@ -21,6 +21,7 @@ def main():
         title="Select BRANESIC file.",
         defaultextension=".brans",
     )
+    if type(input_path) != str: raise(Exception("User did not input a valid path !"))
     output_text = ""
     try:
         output_text = COMPILE(input_path)
@@ -29,9 +30,9 @@ def main():
         print(str(type(e))+": "+str(e))
         return "error"
     
-    if output_path == "" and CONFIG["Output to file ?"]: output_path = filedialog.asksaveasfile(
-        title="It is done. Where to put it ?"
-    ).name
+    if output_path == "" and CONFIG["Output to file ?"]:
+        output_path = filedialog.asksaveasfile(title="It is done. Where to put it ?").name
+        if type(output_path) != str: raise(Exception("User did not input a valid path !"))
 
     if CONFIG["Copy output to clipboard ?"]:clipboard.copy
     if CONFIG["Output to file ?"]: open(output_path, "w").write(output_text)
