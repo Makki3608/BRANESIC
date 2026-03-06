0a6ee6099533ba1e4e2a1764381714a3b954127b
Makki3608 <wulin007@seznam.cz>
2026-03-06 09:52:05 +0100

    oops

--- bnf_dictionary.py
+++ bnf_dictionary.py
@@ -57,7 +57,7 @@ Function_Dictionary = {
     "!save!": "_0_^v",#these let you save 1 number to persistent memory. A better way to do that is TODO. These will stay for simple tasks but they might break the new way ( maybe )
     "!load!": ">>^<<v",
     "!save2!": ">_0_^v<",
-    "load2": ">>^<v<~1~",
+    "!load2!": ">>^<v<~1~",
     "!boom!": ",t:0,+999999999.",#exits with nine 9s. Set destroy value to 999999999 for it to destroy iteslf. #TODO find a better way to do self-destruction.
 }
 
